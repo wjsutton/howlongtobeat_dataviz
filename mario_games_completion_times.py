b1ab2332e14afe440a5b88b6fd58bdfeb91e1d27
Will Sutton <sutton_999@hotmail.com>
2021-04-12 20:10:19 +0100

    reducing get data function

--- mario_games_completion_times.py
+++ mario_games_completion_times.py
@@ -7,13 +7,8 @@ import pandas as pd
 
 def get_game_data(id):
     result = HowLongToBeat().search_from_id(id)
-    columns = ['game_id','game_name','game_image_url','game_web_link','gameplay_main','gameplay_main_unit','gameplay_main_label',
-    'gameplay_main_extra','gameplay_main_extra_unit','gameplay_main_extra_label','gameplay_completionist','gameplay_completionist_unit','gameplay_completionist_label']
-
-    rows = [[result.game_id,result.game_name,result.game_image_url,result.game_web_link,result.gameplay_main,result.gameplay_main_unit,result.gameplay_main_label,
-    result.gameplay_main_extra,result.gameplay_main_extra_unit,result.gameplay_main_extra_label,result.gameplay_completionist,result.gameplay_completionist_unit,result.gameplay_completionist_label]]
-
-    df = pd.DataFrame(rows, columns = columns)
+    a = result.__dict__
+    df = pd.DataFrame([a])
     return df
 
 
@@ -40,6 +35,9 @@ output_df.game_id = output_df.game_id.astype(int)
 games.ID = games.ID.astype(int)
 
 output_df = pd.merge(output_df, games, left_on='game_id', right_on='ID', how='inner')
+
+# replace halves with 0.5s
+output_df = output_df.replace('½','.5', regex=True)
 
 output_df.to_csv('data\\fastest_mario.csv', encoding="utf-8-sig", index=False)
 
